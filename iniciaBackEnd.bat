95d484bc44e62005b2f845269536dc9ac646fa76
ManuelCoral <79032745+ManuelCoral@users.noreply.github.com>
2022-01-10 22:07:01 -0600

    Add files via upload

---
+++ iniciaBackEnd.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ** Iniciando Servidor BackEnd**
+symfony server:start --port=80
+echo ** Iniciado **
+pause
+exit
---
+++ iniciaFrontEnd.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ** Iniciando Servidor Front End**
+cd .\front_end\vue\  
+call npm run dev
+echo ** Iniciado **
+pause
+exit
